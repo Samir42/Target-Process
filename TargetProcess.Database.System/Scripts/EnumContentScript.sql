9df41b9f81393b33110c495b750f4c1d38fac217
Samir Osmanov <samiro@emgage.com>
2021-04-25 11:34:58 +0400

    Architecture organization / User-Tenant DB schema applied

---
+++ TargetProcess.Database.System/Scripts/EnumContentScript.sql
@@ -0,0 +1,10 @@
+﻿----------------
+-- Tiny Enums --
+----------------
+
+-- Do these first so the other tables who have enum foreign keys can reference this during insertion
+
+INSERT INTO [TinyEnum].[LoginStatus]
+	SELECT 1, 'LoginEnabled', NULL UNION
+	SELECT 2, 'SetupPending', NULL UNION
+	SELECT 3, 'LoginDisabled', NULL 
---
+++ TargetProcess.Database.System/Tables/TinyEnum/LoginStatus.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [TinyEnum].[LoginStatus]
+(
+	ID TINYINT PRIMARY KEY,	
+	[Name] VARCHAR(128) NOT NULL,
+	[Description] NVARCHAR(512) DEFAULT NULL,
+	CONSTRAINT c_LoginStatus_Name_Unique UNIQUE ([Name])
+)
